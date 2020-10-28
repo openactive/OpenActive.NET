4d71a625aaf3eb5aed3b3a5671f1616d4a7d8378
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-10-28 23:13:44 +0000

    12.5.7 - Update test interface

--- OpenActive.NET/enums/TestOpportunityCriteriaEnumeration.cs
+++ OpenActive.NET/enums/TestOpportunityCriteriaEnumeration.cs
@@ -16,14 +16,14 @@ namespace OpenActive.NET
         TestOpportunityNotBookableViaAvailableChannel,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNoSpaces")]
         TestOpportunityBookableNoSpaces,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableTwoSpaces")]
-        TestOpportunityBookableTwoSpaces,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFiveSpaces")]
         TestOpportunityBookableFiveSpaces,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFree")]
         TestOpportunityBookableFree,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaid")]
-        TestOpportunityBookablePaid,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFree")]
+        TestOpportunityBookableNonFree,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableUsingPayment")]
+        TestOpportunityBookableUsingPayment,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableWithinValidFromBeforeStartDate")]
         TestOpportunityBookableWithinValidFromBeforeStartDate,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableOutsideValidFromBeforeStartDate")]
@@ -40,30 +40,22 @@ namespace OpenActive.NET
         TestOpportunityBookableOutsideValidFromBeforeStartDateWindow,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFreeAttendeeDetails")]
         TestOpportunityBookableFreeAttendeeDetails,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidAttendeeDetails")]
-        TestOpportunityBookablePaidAttendeeDetails,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableAdditionalDetails")]
         TestOpportunityBookableAdditionalDetails,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFreePrepaymentOptional")]
         TestOpportunityBookableFreePrepaymentOptional,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidPrepaymentOptional")]
-        TestOpportunityBookablePaidPrepaymentOptional,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFreePrepaymentUnavailable")]
-        TestOpportunityBookableFreePrepaymentUnavailable,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidPrepaymentUnavailable")]
-        TestOpportunityBookablePaidPrepaymentUnavailable,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFreePrepaymentOptional")]
+        TestOpportunityBookableNonFreePrepaymentOptional,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFreePrepaymentUnavailable")]
+        TestOpportunityBookableNonFreePrepaymentUnavailable,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFreePrepaymentRequired")]
         TestOpportunityBookableFreePrepaymentRequired,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidPrepaymentRequired")]
-        TestOpportunityBookablePaidPrepaymentRequired,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFreePrepaymentUnspecified")]
-        TestOpportunityBookableFreePrepaymentUnspecified,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidPrepaymentUnspecified")]
-        TestOpportunityBookablePaidPrepaymentUnspecified,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidTaxGross")]
-        TestOpportunityBookablePaidTaxGross,
-        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookablePaidTaxNet")]
-        TestOpportunityBookablePaidTaxNet,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFreePrepaymentRequired")]
+        TestOpportunityBookableNonFreePrepaymentRequired,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFreeTaxGross")]
+        TestOpportunityBookableNonFreeTaxGross,
+        [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableNonFreeTaxNet")]
+        TestOpportunityBookableNonFreeTaxNet,
         [EnumMember(Value = "https://openactive.io/test-interface#TestOpportunityBookableFlowRequirementOnlyApproval")]
         TestOpportunityBookableFlowRequirementOnlyApproval
     }
